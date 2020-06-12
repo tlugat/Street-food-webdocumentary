7045778982d921ceddbfd2d30edbb6014df65b7e
Teo Lugat <teo.lugat@hetic.net>
2020-06-12 12:48:12 +0200

    responsive about brands

---
+++ src/Utils/TheAbout/AboutBrands.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="about">
+    <div class="about__section about__section--1">
+      <div class="about__titleWrapper">
+        <h3 class="about__title">À propos du projet</h3>
+        <div class="about__square"></div>
+      </div>
+      <p>
+        Thalassius vero ea tempestate praefectus praetorio praesens ipse quoque
+        adrogantis ingenii, considerans incitationem eius ad multorum augeri
+        discrimina, non maturitate vel consiliis mitigabat.
+      </p>
+    </div>
+    <div class="about__section about__section--2">
+      <div class="about__titleWrapper">
+        <h3 class="about__title">Crédits</h3>
+        <div class="about__square"></div>
+      </div>
+      <div class="credits">
+        <div class="credits__part">
+          <div class="credits__item">
+            <p>Téo Lugat</p>
+            <p class="expertise">Front-end</p>
+          </div>
+          <div class="credits__item">
+            <p>Kylian Mendy</p>
+            <p class="expertise">Design/Front-end</p>
+          </div>
+          <div class="credits__item">
+            <p>Wendy Paulos</p>
+            <p class="expertise">Design/Front-end</p>
+          </div>
+        </div>
+        <div class="credits__part">
+          <div class="credits__item">
+            <p>Michael Vieira</p>
+            <p class="expertise">Design/Front-end</p>
+          </div>
+          <div class="credits__item">
+            <p>Emmy Peteytas</p>
+            <p class="expertise">Design/Front-end</p>
+          </div>
+          <div class="credits__item">
+            <p>Imene Ben Rachid</p>
+            <p class="expertise">Design/Front-end</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="trait"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/global.scss";
+
+.about {
+  padding: 2rem 1rem;
+  position: relative;
+  // font-family: poppins-regular;
+  @include large {
+    display: flex;
+  }
+  &__section {
+    @include large {
+      width: 50%;
+      padding: 2rem;
+    }
+  }
+  &__titleWrapper {
+    position: relative;
+    margin-bottom: 1rem;
+  }
+  &__square {
+    height: 1rem;
+    width: 1rem;
+    background: #f7b200;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+}
+.about__section--1 {
+  margin-bottom: 2rem;
+  @media (min-width: 1024px) {
+    width: 40%;
+  }
+}
+// .about__section--2 {
+//   @media (min-width: 500px) {
+//     width: 75%;
+//   }
+// }
+.credits {
+  display: flex;
+  @include small {
+    @include flex(column, null, center);
+  }
+  @include medium {
+    width: 30rem;
+    justify-content: center;
+  }
+  &__part {
+    margin-right: 1.5rem;
+    @include small {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      margin: 0.5rem 0;
+    }
+  }
+  &__item {
+    @include flex(column, null, center);
+    margin-bottom: 1rem;
+  }
+}
+.expertise {
+  font-size: 12px;
+}
+.trait {
+  height: 8px;
+  background: #6ad051;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
--- src/Views/LandingPage.vue
+++ src/Views/LandingPage.vue
@@ -1,7 +1,93 @@
-<template></template>
+<template>
+  <div>
+    <about-brands class="about__brands"></about-brands>
+    <p class="about__btn" @click="slideAbout">ABOUT</p>
+    <div class="disclaimer">
+      <div class="disclaimer__textWrapper">
+        <svg
+          width="22"
+          height="22"
+          viewBox="0 0 22 22"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11 21.8337C5.01677 21.8337 0.166687 16.9836 0.166687 11.0003C0.166687 5.01708 5.01677 0.166992 11 0.166992C16.9833 0.166992 21.8334 5.01708 21.8334 11.0003C21.8334 16.9836 16.9833 21.8337 11 21.8337ZM11 19.667C13.2986 19.667 15.503 18.7539 17.1283 17.1286C18.7536 15.5033 19.6667 13.2989 19.6667 11.0003C19.6667 8.70178 18.7536 6.49738 17.1283 4.87207C15.503 3.24675 13.2986 2.33366 11 2.33366C8.70148 2.33366 6.49708 3.24675 4.87176 4.87207C3.24645 6.49738 2.33335 8.70178 2.33335 11.0003C2.33335 13.2989 3.24645 15.5033 4.87176 17.1286C6.49708 18.7539 8.70148 19.667 11 19.667ZM9.91669 14.2503H12.0834V16.417H9.91669V14.2503ZM9.91669 5.58366H12.0834V12.0837H9.91669V5.58366Z"
+            fill="#ECECEC"
+          />
+        </svg>
+        <p class="disclaimer__text">
+          "Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus
+          Bachelor de l’école HETIC. Les contenus présentés n'ont pas fait
+          l'objet d'une demande de droit d'utilisation. Ce site ne sera en aucun
+          cas exploité à des fins commerciales et ne sera pas publié”
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import AboutBrands from "../Utils/TheAbout/AboutBrands";
+export default {
+  components: {
+    AboutBrands,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    slideAbout() {
+      document.querySelector(".about__brands").classList.toggle("open");
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/global.scss";
+.menu {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.about {
+  &__brands {
+    transform: translateY(-100%);
+    transition: 1s;
+  }
+  &__brands.open {
+    transform: translateY(0);
+  }
+  &__btn {
+    font-family: poppins-semiBold;
+    cursor: pointer;
+    position: absolute;
+    top: 1rem;
+    right: 2rem;
+  }
+}
+.disclaimer {
+  width: 10rem;
+  padding: 2rem 3rem;
+  position: absolute;
+  bottom: 0;
+  font-family: poppins-regular;
+  font-size: 10px;
+  @include small {
+    width: 20rem;
+  }
+  @include medium {
+    width: 30rem;
+    font-size: 12px;
+  }
+  &__textWrapper {
+    position: relative;
+    svg {
+      position: absolute;
+      top: 0;
+      left: -2rem;
+    }
+  }
+}
+</style>
