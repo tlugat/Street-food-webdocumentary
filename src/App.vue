efabb42d60dffc7bb2b63379b609fe785430e543
Teo Lugat <teo.lugat@hetic.net>
2020-06-12 13:14:52 +0200

    new src folder

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,154 @@
 <template>
   <div id="app">
-    <Menu class="menu"></Menu>
-    <LeSaviezVous class="anecdote"></LeSaviezVous>
-    <AboutBrands class="about-brands"></AboutBrands>
+    <landing-page class="view"></landing-page>
   </div>
 </template>
 
 <script>
-import Menu from "./components/Utils/TheMenu/Menu";
-import LeSaviezVous from "./components/Utils/LeSaviezVous/LeSaviezVous";
-import AboutBrands from "./components/Utils/TheAbout/AboutBrands";
-
+import LandingPage from "./Views/LandingPage";
 export default {
   name: "App",
   components: {
-    Menu,
-    LeSaviezVous,
-    AboutBrands
-  }
+    LandingPage,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #app {
-  .menu {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-  }
+  height: 100vh;
+  width: 100vw;
+  font-family: calibri;
+}
+.view {
+  height: 100vh;
+}
+</style>
 
-  .anecdote {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-
-  .about-brands {
-    position: absolute;
-    top: 150px;
-  }
+<style lang="scss">
+// reset
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 </style>
---
+++ src/Utils/TheMenu/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <box-menu></box-menu>
+</template>
+
+<script>
+import BoxMenu from "./MenuBox/Box";
+export default {
+  components: {
+    BoxMenu,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
