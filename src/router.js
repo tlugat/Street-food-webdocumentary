856e8144271735eaa85a69155e4657626f31eb62
Teo Lugat <teo.lugat@hetic.net>
2020-06-17 20:35:07 +0200

    router

--- src/router.js
+++ src/router.js
@@ -5,15 +5,13 @@ import IntroChapitre from "./components/IntroChapitre.vue";
 import Recettes from "./components/Recettes.vue";
 import FullText from "./components/FullText";
 import HalfText from "./components/HalfText";
-import JayFai from "./components/Jay-Fai";
+import JayFai from "./components/JayFaiComponents/Jay-Fai";
 import Stats from "./components/Stats";
 import LandingPage from "./Views/LandingPage";
-import Video from "./components/Video";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  mode: "history",
   routes: [
     {
       path: "/",
@@ -25,7 +23,7 @@ export default new VueRouter({
       component: Chefs,
     },
     {
-      path: "/intro-chapitre",
+      path: "/intro-chapitre/:id",
       component: IntroChapitre,
     },
     {
@@ -34,69 +32,16 @@ export default new VueRouter({
       component: Recettes,
     },
     {
-      path: "/video",
-      name: "video",
-      component: Video,
-    },
-    {
-      path: "/origines",
-      name: "origines",
+      path: "/text/:id",
       component: HalfText,
       props: true,
     },
     {
-      path: "/origines/2",
+      path: "/full-text/:id",
       component: FullText,
       props: true,
-      params: {
-        text1: `La cuisine de rue accompagne l’évolution de nos sociétés au fil des siècles et emprunte différentes formes selon le lieu et l’époque. Pour certains pays, elle se fait moins présente tandis que pour d’autres elle constitue un secteur lucratif et une source importante d’emplois.`,
-        text2: `Les classes urbaines et défavorisées de l’époque partageaient leur habitation et ne disposaient pas pour la plupart d’une cuisine. Ils devaient manger dans la rue en se rendant dans un thermopolium (un établissement de restauration rapide qui proposait des repas pour toutes les bourses).`,
-        background: `https://cdn.discordapp.com/attachments/697362929946722358/722084316250636288/img-apogee-desktop.png`,
-      },
     },
 
-    {
-      path: "/today",
-      name: "today",
-      component: HalfText,
-      props: true,
-    },
-    {
-      path: "/today/2",
-      name: "today2",
-      component: FullText,
-      props: true,
-      params: {
-        text1: `Les plats sont diverses et tirent leur origine de la culture locale, internationale, un mélange des deux ou bien de la créativité de celui ou celle qui les réalise.
-        `,
-        text2: `Autour de ces restaurants en plein air, souvent peu voire pas de places pour les clients mais pourtant ils sont légion à tenter l’expérience culinaire.`,
-        background: `https://cdn.discordapp.com/attachments/697362929946722358/722152896258375740/background-today2_1.png`,
-      },
-    },
-    {
-      path: "/recette",
-      name: "recette",
-      component: HalfText,
-      props: true,
-    },
-    {
-      path: "/chic",
-      name: "chic",
-      component: HalfText,
-      props: true,
-    },
-    {
-      path: "/conclusion",
-      name: "conclusion",
-      component: FullText,
-      props: true,
-    },
-    {
-      path: "/fine-casual",
-      name: "fine-casual",
-      component: HalfText,
-      props: true,
-    },
     {
       path: "/stats",
       component: Stats,
